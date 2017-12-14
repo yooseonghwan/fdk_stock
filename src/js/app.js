990f7224915e1572af406d9f2f8552c2dd1a6576
yoo1984 <gt1984@naver.com>
2017-12-14 17:04:55 +0900

    12/14

--- src/js/app.js
+++ src/js/app.js
@@ -26,15 +26,30 @@ function Emoticon(name,profile,id,img) {
   this.img = img;
 }
 
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+  }
+  return "";
+}
 
 app.controller('listController',['$scope','$http', function($scope,$http){
-  
-
-    var today = new Date();
-    $scope.today = today; 
-
-   // $scope.startAjax = function(){
-
 
     $http({
     method: 'GET' ,
@@ -76,11 +91,16 @@ app.controller('listController',['$scope','$http', function($scope,$http){
         console.log('Complete');
     });
 
+    var today = new Date('YYYY-MM-DD');
+    $scope.today = today; 
+
+    var endDay   = today;
+    var startDay = today.setDate(today - 30);
 
     //일자별 
     $http({
       method: 'GET' ,
-      url: 'https://api.intrinio.com/prices?identifier=FDC&start_date=2017-11-01&end_date=2017-12-13',
+      url: 'https://api.intrinio.com/prices?identifier=FDC&start_date='+startDay+'&end_date='+endDay,
    
       headers: {
           "Authorization": auth
@@ -137,63 +157,26 @@ app.controller('listController',['$scope','$http', function($scope,$http){
           console.log('Complete');
       });
 
-    
-      
+      $scope.change = function() {
+            
+        setCookie("count",$scope.get_stock,365);
+      }
+     
 
-
-
-   //과거 api 가지고 오기 
-    //   // 환율 가지고 오기 
-    //   var httpPromise = $http({
-    //     "method":"get",      
-    //    //  "url":"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20csv%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fd%2Fquotes.csv%3Fe%3D.csv%26f%3Dc4l1%26s%3DUSDKRW%3DX%22%3B&format=json&diagnostics=true&callback="
-    //      "url" :"http://api.fixer.io/latest?base=USD"
-    //   });
-
-    //   httpPromise.success(function(data,status,
-    //     header,config) {
-
-    //     var currency_amt = data.query.results.row.col1;
-    //     $scope.currency_amt = currency_amt;
-    //   });
-
-    //   httpPromise.error(function(data,status,header,config){
-    //       alert("환율 실패");
-    //   });
-
-    //   var username = "3b079ff3387899bfae4c599ce5ea14c9";
-    //   var password = "a64d5125c63f9953384f5937ddbde822";
-    //   var auth = "Basic " + (username + ':' + password) .toString('base64');
-    // //주식 정보 가지고 오기 
-    //   var httpPromise2 = $http({
-    //     "method":"get",      
-    //   //   "url":"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%3D%22FDC%22%20&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
-    //       "url":"https://api.intrinio.com/data_point?identifier=FDC&item=ask_price" ,
+      var getCookie = getCookie("count");
+      if(getCookie ==""){
+        $scope.get_stock="300";
+        $scope.get_stock_value="300";
+      }else{
+        $scope.get_stock= getCookie("count");
+        $scope.get_stock_value= getCookie("count");
+      }
+     
     
 
-    // });
 
-    //   //성공시 호출되는 콜백
-    //   httpPromise2.success(function(data,status,
-    //     header,config) {
-
-    //     var stock_amt =  data.query.results.quote.LastTradePriceOnly;
-    //     var Change =  data.query.results.quote.Change;
-       
-        
-    //     $scope.stock_amt = stock_amt;
-    //     $scope.Change = Change;
-       
-    //   });
-
-    //   //실패시 호출되는 콜백
-    //   httpPromise2.error(function(data,status,header,config){
-    //      alert(errors);
-    //       alert("주식 실패");
-    //   });
-
-  
 }]);
+
 
 
 app.controller('detailController', [
